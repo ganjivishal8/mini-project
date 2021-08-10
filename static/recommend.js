d2f6ac6df8d0c99c0c86aa9e6915a8ce250d0464
vishal <ganjivishal8@gmail.com>
2021-08-10 09:30:16 +0530

    commits

--- static/recommend.js
+++ static/recommend.js
@@ -1,3 +1,60 @@
+$(document).ready(function() {
+var $imagesCarousel = $('.carouselOfImages').flickity({
+  contain: true,
+  autoPlay: true,
+  wrapAround: true,
+  friction: 0.3
+});
+function resizeCells() {
+  var flkty = $imagesCarousel.data('flickity');
+  var $current = flkty.selectedIndex
+  var $length = flkty.cells.length
+  if ($length <='5') {
+    $imagesCarousel.flickity('destroy');
+  }
+  $('.carouselOfImages .carouselImage').removeClass("nextToSelected");
+  $('.carouselOfImages .carouselImage').eq($current-1).addClass("nextToSelected");
+  if ($current+1 == $length) {
+    var $endCell = "0"
+  } else {
+    var $endCell = $current+1
+  }
+   $('.carouselOfImages .carouselImage').eq($endCell).addClass("nextToSelected");
+  };
+resizeCells();
+
+$imagesCarousel.on('scroll.flickity', function() {
+    resizeCells();
+  });
+
+
+
+
+
+$(".carouselImage img").click(function() {
+  var $this = $(this);
+  var imageID = $this.attr('data-tab');
+  var imageSrc = $this.attr('src');
+
+  $('.' + imageID).removeClass('hide');
+  $('.' + imageID + ' .product-detail-image img').attr('src', imageSrc);
+});
+
+$('.product-detail-close,.product-detail').on('click', function() {
+  $('.product-detail').addClass('hide');
+});
+
+  $('.modal-video').on('hidden.bs.modal', function (e) {
+    $('.modal-video iframe').attr('src', $('.modal-video iframe').attr('src'));
+  });
+
+$(window).on('load resize', function(){
+  var $window = $(window);
+  $('.modal-fill-vert .modal-body > *').height(function(){
+      return $window.height()-60;
+    });
+  });
+});
 $(function() {
   // Button will be disabled until we type something inside the input field
   const source = document.getElementById('autoComplete');
